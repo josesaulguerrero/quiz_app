bb58a0a9b68312695369a0fce30004391c97ecf9
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 17:32:44 -0500

    server: modeled the answer entity

---
+++ server/src/modules/answers/answer.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+//local modules
+import { Question } from '../questions/question.entity';
+
+@Entity()
+export class Answer {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ type: 'varchar', length: 255 })
+	text: string;
+
+	@Column({ type: 'boolean' })
+	isCorrect: boolean;
+
+	@ManyToOne(() => Question, (question) => question.answers)
+	question: Question;
+}
