7690d509426c7cc105ef4af35eb29383db061f21
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 15:14:20 -0500

    set up the database migrations

---
+++ server/ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+	type: 'postgres',
+	url: process.env.DATABASE_URL,
+	synchronize: false,
+	logging: false,
+	migrations: ['src/modules/database/migrations/*.ts'],
+	migrationsTableName: 'migrations',
+	entities: ['src/**/*.entity.ts'],
+	cli: {
+		migrationsDir: 'src/modules/database/migrations',
+	},
+	ssl: {
+		rejectUnauthorized: false,
+	},
+};
