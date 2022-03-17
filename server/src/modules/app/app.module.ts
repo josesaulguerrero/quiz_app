a32c4a7e83a8f83836c8e15d737f2171c245fd16
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 15:34:32 -0500

    set up the config module to read and validate environment variables

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+//local modules
+import { DatabaseModule } from '../database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		DatabaseModule,
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+			isGlobal: true,
+			validationSchema: Joi.object({
+				DATABASE_URL: Joi.string().uri().required(),
+				PORT: Joi.number(),
+			}),
+			validationOptions: {
+				allowUnknown: true,
+				abortEarly: false,
+			},
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
