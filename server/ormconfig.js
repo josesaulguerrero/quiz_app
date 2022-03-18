5d0c520975b401b17d804a1714a53c52daa38af0
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-18 10:08:20 -0500

    server: fix - the remote database isn't running the migrations

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
 	logging: false,
 	migrations: ['src/modules/database/migrations/*.ts'],
 	migrationsTableName: 'migrations',
-	entities: ['src/**/*.entity.ts'],
+	entities: ['./src/**/*.entity.ts'],
 	cli: {
 		migrationsDir: 'src/modules/database/migrations',
 	},
