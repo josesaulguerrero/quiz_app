7b1735e03cd197bbdb2ad599a6a788463f15f864
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-18 11:48:34 -0500

    server: added the logic behind the player service and controller

--- server/src/modules/players/players.controller.ts
+++ server/src/modules/players/players.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreatePlayerDTO } from './player.dto';
 import { PlayersService } from './players.service';
 
 @Controller('players')
 export class PlayersController {
 	constructor(private readonly playersService: PlayersService) {}
+
+	@Get()
+	findAll() {
+		return this.playersService.findAll();
+	}
+
+	@Post()
+	create(@Body() playerData: CreatePlayerDTO) {
+		return this.playersService.create(playerData);
+	}
 }
--- server/src/modules/players/players.service.ts
+++ server/src/modules/players/players.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreatePlayerDTO } from './player.dto';
 import { Player } from './player.entity';
 
 @Injectable()
@@ -9,4 +10,13 @@ export class PlayersService {
 		@InjectRepository(Player)
 		private readonly playerRepository: Repository<Player>,
 	) {}
+
+	findAll() {
+		return this.playerRepository.find();
+	}
+
+	create(playerData: CreatePlayerDTO) {
+		const newPlayer = this.playerRepository.create(playerData);
+		return this.playerRepository.save(newPlayer);
+	}
 }
