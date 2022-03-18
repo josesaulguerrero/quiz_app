cf950d912a2429b67445c28f1af25b3ad531d249
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-18 16:39:09 -0500

    client: modeled the structure for the game context

---
+++ client/src/context/game.context.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from 'react';
+
+const GameContext = createContext<string>(null!);
+
+const GameContextProvider = () => {
+	const [gameState, setGameState] = useState<gameStates>(gameStates.INITIAL);
+	const [hasWon, setHasWon] = useState<boolean>(false);
+	const [round, setRound] = useState<gameRounds>(gameRounds.FIRST);
+	const [points, setPoints] = useState<number>(0);
+	const categories = ''; //we'll fetch some data later
+	const questions = '';
+	/*
+      1. set the initial config (username)
+      2. fetch categories
+      3. start game
+      4. show questions
+      5. set game over if the player loses or wins
+      6. show hall of fame if the player won
+      7. show game over screen if the player lost
+   */
+	return <GameContext.Provider value={'hello'}></GameContext.Provider>;
+};
---
+++ client/src/global.d.ts
@@ -0,0 +1,13 @@
+enum gameStates {
+	INITIAL = 'INITIAL',
+	PLAYING = 'PLAYING',
+	GAME_OVER = 'GAME_OVER',
+}
+
+enum gameRounds {
+	FIRST = 1,
+	SECOND = 2,
+	THIRD = 3,
+	FOURTH = 4,
+	FIFTH = 5,
+}
