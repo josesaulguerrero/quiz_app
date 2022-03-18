f8adf0b276b5dde032371bca70e77d7738525522
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 19:48:56 -0500

    server: set up global validation pipes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			forbidUnknownValues: true,
+		}),
+	);
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
