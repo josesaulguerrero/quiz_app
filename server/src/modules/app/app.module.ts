f85557d6597d3e4293737fbb99ea57efb0588947
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 19:07:26 -0500

    server: fix - imported the required modules from the app.module

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { AnswersModule } from '../answers/answers.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { DatabaseModule } from '../database/database.module';
+import { QuestionsModule } from '../questions/questions.module';
 //local modules
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
 	imports: [
+		QuestionsModule,
+		AnswersModule,
+		CategoriesModule,
+		DatabaseModule,
 		ConfigModule.forRoot({
 			envFilePath: '.env',
 			isGlobal: true,
