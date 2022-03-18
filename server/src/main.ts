0288a6cc3362272671363daa27b8f1ca50bd894e
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-18 10:30:26 -0500

    server: set the port to be dynamic

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
 			forbidUnknownValues: true,
 		}),
 	);
-	await app.listen(process.env.PORT || 3000);
+	await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
