13d3923131f782a0c0558d82806cf7a128436993
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-18 09:29:54 -0500

    server: setting up to deploy

--- server/src/modules/database/database.module.ts
+++ server/src/modules/database/database.module.ts
@@ -9,11 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 			useFactory: (configService: ConfigService) => {
 				return {
 					type: 'postgres',
-					host: configService.get('DB_HOST'),
-					port: configService.get('DB_PORT'),
-					username: configService.get('DB_USER'),
-					password: configService.get('DB_PASSWORD'),
-					database: configService.get('DB_NAME'),
+					url: configService.get('DATABASE_URL'),
 					synchronize: false,
 					autoLoadEntities: true,
 				};
