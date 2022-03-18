093b8da6fa2cecfc3f4767a036a39bd2fc76e7f5
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-17 19:49:23 -0500

    server: finished the categories controller! Seems to be working properly

--- server/src/modules/categories/categories.controller.ts
+++ server/src/modules/categories/categories.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDTO, UpdateCategoryDTO } from './category.dto';
 
 @Controller('categories')
 export class CategoriesController {
 	constructor(private readonly categoriesService: CategoriesService) {}
+
+	@Get()
+	findAll() {
+		return this.categoriesService.findAll();
+	}
+
+	@Get(':categoryId')
+	findOne(@Param('categoryId', ParseIntPipe) categoryId: number) {
+		return this.categoriesService.findOneById(categoryId);
+	}
+
+	@Post()
+	create(@Body() categoryData: CreateCategoryDTO) {
+		return this.categoriesService.create(categoryData);
+	}
+
+	@Patch(':categoryId')
+	update(
+		@Param('categoryId', ParseIntPipe) categoryId: number,
+		@Body() updatedData: UpdateCategoryDTO,
+	) {
+		return this.categoriesService.update(categoryId, updatedData);
+	}
+
+	@Delete(':categoryId')
+	delete(@Param('categoryId', ParseIntPipe) categoryId: number) {
+		return this.categoriesService.delete(categoryId);
+	}
 }
