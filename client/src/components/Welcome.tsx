19dd22ccae2899a8091bbf0e6ffa98b7db5d9082
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-19 11:43:05 -0500

    client: added the logic to avoid repeated usernames

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,20 +1,36 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 // local modules
 import { gameStates, IGameContext } from '../@types';
 import { GameContext } from '../context/game.context';
+import { useFetch } from '../hooks/useFetch';
 
 export const Welcome: FC = () => {
-	const [inputData, setInputData] = useState<string>('');
 	const { setUsername, setGameState } = useContext<IGameContext>(GameContext);
+	const [inputData, setInputData] = useState<string>('');
+	const [nicknameIsAvailable, setNicknameIsAvailable] = useState<boolean>(true);
+	const [playerRequestState, fetchPlayer] = useFetch(
+		`${process.env.REACT_APP_API_BASE_URL}/players?name=${inputData}`
+	);
+	const { data } = playerRequestState;
 
 	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setNicknameIsAvailable(true);
 		setInputData(event.target.value);
 	};
 
 	const onClick = () => {
+		fetchPlayer();
 		setUsername(inputData);
 		setGameState(gameStates.PLAYING);
 	};
+
+	useEffect(() => {
+		if (data) {
+			(data as Array<unknown>).length === 0
+				? setNicknameIsAvailable(true)
+				: setNicknameIsAvailable(false);
+		}
+	}, [playerRequestState]);
 
 	return (
 		<section className="welcome">
@@ -25,14 +41,17 @@ export const Welcome: FC = () => {
 					type="text"
 					name="nickname"
 					id="nickname"
-					placeholder="E.G. Alex92"
+					placeholder="e.g. Alex92"
 					onChange={onChange}
 				/>
 			</label>
+			{(data as Array<unknown>)?.length >= 1 && (
+				<p className="error">username is not available...</p>
+			)}
 			<button
 				className="welcomeButton"
 				type="button"
-				disabled={!inputData.trim()}
+				disabled={!inputData.trim() || !nicknameIsAvailable}
 				onClick={onClick}
 			>
 				Let&apos;s get started!
