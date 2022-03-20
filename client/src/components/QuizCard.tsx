601a19c99e403d1e2a750875fd25b6bce543bd68
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-19 19:51:02 -0500

    client: tried to randomize the questions even more :p

--- client/src/components/QuizCard.tsx
+++ client/src/components/QuizCard.tsx
@@ -31,9 +31,9 @@ export const QuizCard: FC = () => {
 
 	useEffect(() => {
 		if (categoryRequestState.data) {
-			// when the data is ready, we will pick up a random question from the given category
+			// when the data is ready, we will shuffle the questions and then pick up a random question from the given category
 			const randomQuestionFromCategory = randomElementFromArray<IQuestion>(
-				(categoryRequestState.data as ICategory).questions
+				shuffleArray((categoryRequestState.data as ICategory).questions)
 			);
 			// we need to shuffle the answers to avoid the players from cheating!
 			const shuffledAnswers = shuffleArray(randomQuestionFromCategory.answers);
