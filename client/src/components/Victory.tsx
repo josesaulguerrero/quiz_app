7d10568601843955c150c662c488ae907a6a2e57
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2022-03-19 22:38:49 -0500

    client: added a button on the victory screen, so that users can go back to the main page

--- client/src/components/Victory.tsx
+++ client/src/components/Victory.tsx
@@ -1,12 +1,30 @@
 import React, { FC, useContext } from 'react';
 // local modules
-import { IGameContext } from '../@types';
+import { gameRounds, gameStates, IGameContext } from '../@types';
 import { GameContext } from '../context/game.context';
 import '../styles/victory.styles.css';
 import { HallOfFame } from './HallOfFame';
 
 export const Victory: FC = () => {
-	const { points, round } = useContext<IGameContext>(GameContext);
+	const {
+		points,
+		round,
+		setGameState,
+		setHasWon,
+		setPoints,
+		setUsername,
+		setRound,
+	} = useContext<IGameContext>(GameContext);
+
+	const onGoBackToWelcomeScreen = () => {
+		// we need to reset EVERYTHING before going to the welcome home
+		setHasWon(false);
+		setPoints(0);
+		setUsername('');
+		setRound(gameRounds.FIRST);
+		setGameState(gameStates.INITIAL);
+	};
+
 	return (
 		<section className="victory">
 			<h2 className="victoryTitle">You won :D</h2>
@@ -14,6 +32,13 @@ export const Victory: FC = () => {
 				You reached the round {round} and got {points} points
 			</p>
 			<HallOfFame />
+			<button
+				onClick={() => onGoBackToWelcomeScreen()}
+				className="goToWelcomeButton"
+				type="button"
+			>
+				Go to the welcome page
+			</button>
 		</section>
 	);
 };
